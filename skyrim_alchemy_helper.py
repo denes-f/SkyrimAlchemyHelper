18f34f6c1628752e07ff9088f6250e8a54e93173
Denes <dns993@gmail.com>
2024-03-28 10:19:37 +0100

    Updated naming and improved user input/output messages

--- skyrim_alchemy_helper.py
+++ skyrim_alchemy_helper.py
@@ -2,62 +2,74 @@ import csv
 
 
 def main():
-    ingredient_list = IngredientList()
+    effect_list = EffectList()
     with open('effects.csv', encoding='utf-8-sig') as effects_file:
         effects_reader = csv.reader(effects_file, delimiter=';')
         for row in effects_reader:
             effect = row[0]
             ingredient = row[1]
-            ingredient_list.append(ingredient, effect)
+            effect_list.add_ingredient(ingredient, effect)
     while True:
-        query = input("Search for an effect of an ingredient: ")
-        query_split = query.rsplit(' ', 1)
+        query_text = input("Search for an effect of an ingredient: ")
+        query_split = query_text.rsplit(' ', 1)
+        if len(query_split) != 2:
+            print(f"Invalid input. Specify the query in the following format: Ingredient Name <space> Effect Order\n")
+            continue
         query_ingredient = query_split[0]
-        query_effect_order = int(query_split[1])
-        queried_effect = ingredient_list.get_effect(query_ingredient, query_effect_order)
-        queried_effect_ingredients = ingredient_list.get_ingredients(queried_effect)
+        try:
+            query_effect_order = int(query_split[1])
+        except ValueError:
+            print(f"Invalid Effect Order '{query_split[1]}'. The Effect Order should be a number\n")
+            continue
+        queried_effect = effect_list.get_effect(query_ingredient, query_effect_order)
+        queried_ingredients = effect_list.get_ingredients(queried_effect)
         print(f"\n{query_ingredient} effect {query_effect_order}: {queried_effect}")
         print("---------------------------------------------------")
-        print(f"Ingredients: {', '.join(queried_effect_ingredients)}\n\n")
+        print(f"Ingredients: {', '.join(queried_ingredients)}\n\n")
 
 
 class Ingredient:
     def __init__(self, name):
-        self.name = name
-        self.effects = []
+        self._name = name
+        self._effects = []
 
     def add_effect(self, effect):
-        self.effects.append(effect)
+        self._effects.append(effect)
 
-    def get_effects(self):
-        return self.effects
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def effects(self):
+        return self._effects
 
 
-class IngredientList:
+class EffectList:
     def __init__(self):
-        self._ingredient_list = []
+        self._ingredients = []
 
-    def append(self, ingredient_name, effect_name):
-        for ingredient in self._ingredient_list:
+    def add_ingredient(self, ingredient_name, effect_name):
+        for ingredient in self._ingredients:
             if ingredient.name == ingredient_name:
                 ingredient.add_effect(effect_name)
                 return
         ingredient = Ingredient(ingredient_name)
         ingredient.add_effect(effect_name)
-        self._ingredient_list.append(ingredient)
+        self._ingredients.append(ingredient)
 
     def get_effect(self, ingredient_name, effect_order):
-        for ingredient in self._ingredient_list:
+        for ingredient in self._ingredients:
             if ingredient.name == ingredient_name:
                 return ingredient.effects[effect_order - 1]
 
     def get_ingredients(self, effect_name):
-        filtered_ingredients = []
-        for ingredient in self._ingredient_list:
+        queried_ingredients = []
+        for ingredient in self._ingredients:
             if effect_name in ingredient.effects:
-                filtered_ingredients.append(ingredient.name)
-        filtered_ingredients.sort()
-        return filtered_ingredients
+                queried_ingredients.append(ingredient.name)
+        queried_ingredients.sort()
+        return queried_ingredients
 
 
 if __name__ == '__main__':
