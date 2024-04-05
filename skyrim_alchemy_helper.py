87a96a2989d8e0c5324c0a66e879c0cede70bf23
Denes <dns993@gmail.com>
2024-04-05 15:57:53 +0200

    Changed data source from .csv to .db. Methods of reading data source updated in skyrim_alchemy_helper.py

--- skyrim_alchemy_helper.py
+++ skyrim_alchemy_helper.py
@@ -1,14 +1,9 @@
 import csv
+import sqlite3
 
 
 def main():
-    effect_list = EffectList()
-    with open('effects.csv', encoding='utf-8-sig') as effects_file:
-        effects_reader = csv.reader(effects_file, delimiter=';')
-        for row in effects_reader:
-            effect = row[0]
-            ingredient = row[1]
-            effect_list.add_ingredient(ingredient, effect)
+    db_handler = DBHandler()
     while True:
         query_text = input("Search for an effect of an ingredient: ")
         query_split = query_text.rsplit(' ', 1)
@@ -21,55 +16,30 @@ def main():
         except ValueError:
             print(f"Invalid Effect Order '{query_split[1]}'. The Effect Order should be a number\n")
             continue
-        queried_effect = effect_list.get_effect(query_ingredient, query_effect_order)
-        queried_ingredients = effect_list.get_ingredients(queried_effect)
+        queried_effect = db_handler.get_effect(query_ingredient, query_effect_order)
+        queried_ingredients = db_handler.get_ingredients(queried_effect)
         print(f"\n{query_ingredient} effect {query_effect_order}: {queried_effect}")
         print("---------------------------------------------------")
         print(f"Ingredients: {', '.join(queried_ingredients)}\n\n")
 
 
-class Ingredient:
-    def __init__(self, name):
-        self._name = name
-        self._effects = []
-
-    def add_effect(self, effect):
-        self._effects.append(effect)
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def effects(self):
-        return self._effects
-
-
-class EffectList:
+class DBHandler:
     def __init__(self):
-        self._ingredients = []
-
-    def add_ingredient(self, ingredient_name, effect_name):
-        for ingredient in self._ingredients:
-            if ingredient.name == ingredient_name:
-                ingredient.add_effect(effect_name)
-                return
-        ingredient = Ingredient(ingredient_name)
-        ingredient.add_effect(effect_name)
-        self._ingredients.append(ingredient)
+        self._table_name = "ingredients"
+        self._db_connection = sqlite3.connect("skyrim_aclhemy.db")
+        self._db_cursor = self._db_connection.cursor()
 
     def get_effect(self, ingredient_name, effect_order):
-        for ingredient in self._ingredients:
-            if ingredient.name == ingredient_name:
-                return ingredient.effects[effect_order - 1]
+        res = self._db_cursor.execute(f"SELECT * FROM {self._table_name} WHERE LOWER(Ingredient) IS '{ingredient_name.lower()}'")
+        effects = res.fetchone()
+        effect = None if effects is None else effects[effect_order]
+        return effect
 
     def get_ingredients(self, effect_name):
-        queried_ingredients = []
-        for ingredient in self._ingredients:
-            if effect_name in ingredient.effects:
-                queried_ingredients.append(ingredient.name)
-        queried_ingredients.sort()
-        return queried_ingredients
+        res = self._db_cursor.execute(f'SELECT Ingredient from {self._table_name} WHERE "{effect_name}" IN ("Primary Effect", "Secondary Effect", "Tertiary Effect", "Quaternary Effect")')
+        ingredients = [row[0] for row in res.fetchall()]
+        ingredients.sort()
+        return ingredients
 
 
 if __name__ == '__main__':
