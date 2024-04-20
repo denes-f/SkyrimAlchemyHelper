b1a2c65cad1fe45786f9dd4e18dd80519cd96e51
Denes Farago <denes.farago@avl.com>
2024-04-20 13:02:54 +0200

    Updated UI

--- skyrim_alchemy_helper.py
+++ skyrim_alchemy_helper.py
@@ -1,42 +1,55 @@
-import csv
 import sqlite3
 
 
 def main():
     db_handler = DBHandler()
+    print("---------------------")
+    print("Skyrim Alchemy Helper")
+    print("---------------------\n")
     while True:
         query_text = input("Search for an effect of an ingredient: ")
         query_split = query_text.rsplit(' ', 1)
         if len(query_split) != 2:
-            print(f"Invalid input. Specify the query in the following format: Ingredient Name <space> Effect Order\n")
+            print(f"Invalid input. Specify the query in the following format: IngredientName <space> EffectOrder\n")
             continue
         query_ingredient = query_split[0]
-        try:
-            query_effect_order = int(query_split[1])
-        except ValueError:
-            print(f"Invalid Effect Order '{query_split[1]}'. The Effect Order should be a number\n")
-            continue
-        queried_effect = db_handler.get_effect(query_ingredient, query_effect_order)
-        queried_ingredients = db_handler.get_ingredients(queried_effect)
-        print(f"\n{query_ingredient} effect {query_effect_order}: {queried_effect}")
-        print("---------------------------------------------------")
-        print(f"Ingredients: {', '.join(queried_ingredients)}\n\n")
+        if query_split[1].lower() == 'a':
+            query_effect_order = [1, 2, 3, 4]
+        else:
+            try:
+                query_effect_order = [int(query_split[1])]
+            except ValueError:
+                print(f"Invalid Effect Order '{query_split[1]}'. "
+                      f"The Effect Order should be a number or 'a' for displaying all effects\n")
+                continue
+        for effect_order in query_effect_order:
+            queried_effect = db_handler.get_effect(query_ingredient, effect_order)
+            queried_ingredients = db_handler.get_ingredients(queried_effect)
+            result_title = f"{query_ingredient.title()} effect {effect_order}: {queried_effect}"
+            print("-" * len(result_title))
+            print(result_title)
+            print("-" * len(result_title))
+            print(f"Ingredients: {', '.join(queried_ingredients)}\n")
+        print("\n")
 
 
 class DBHandler:
     def __init__(self):
-        self._db_connection = sqlite3.connect("skyrim_alchemy.db")
-        self._db_cursor = self._db_connection.cursor()
+        self._connection = sqlite3.connect("skyrim_alchemy.db")
+        self._cursor = self._connection.cursor()
 
     def get_effect(self, ingredient_name, effect_order):
-        res = self._db_cursor.execute('SELECT * FROM ingredients WHERE LOWER(Ingredient) IS ?', (ingredient_name.lower(),))
-        effects = res.fetchone()
+        result = self._cursor.execute('SELECT * FROM ingredients WHERE LOWER(Ingredient) IS ?',
+                                      (ingredient_name.lower(),))
+        effects = result.fetchone()
         effect = None if effects is None else effects[effect_order]
         return effect
 
     def get_ingredients(self, effect_name):
-        res = self._db_cursor.execute('SELECT ingredient FROM ingredients WHERE ? IN ("primary_effect", "secondary_Effect", "tertiary_effect", "quaternary_effect")', (effect_name,))
-        ingredients = [row[0] for row in res.fetchall()]
+        result = self._cursor.execute('SELECT ingredient FROM ingredients WHERE ? IN '
+                                      '("primary_effect", "secondary_Effect", "tertiary_effect", "quaternary_effect")',
+                                      (effect_name,))
+        ingredients = [row[0] for row in result.fetchall()]
         ingredients.sort()
         return ingredients
 
